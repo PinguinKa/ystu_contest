5817a4d418eb567e51a2581c9d395e31dde74290
Covium <JiDIyPing@gmail.com>
2022-11-01 15:43:26 +0300

    Corrected a typo.

--- send_email.py
+++ send_email.py
@@ -25,7 +25,7 @@ def registration(recipient, user_password):
     msg = (MIMEText(template, "html"))
     msg["From"] = sender_name
     msg["To"] = recipient
-    msg["Subject"] = "Регистрация на кафедра иностранных языков ЯГТУ"
+    msg["Subject"] = "Регистрация на кафедру иностранных языков ЯГТУ"
 
     _send(recipient, msg)
 
