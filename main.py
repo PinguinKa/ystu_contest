951ab0bbc94e55a73eba8711c40dbb4e94df866a
Covium <JiDIyPing@gmail.com>
2022-11-04 18:14:15 +0300

    PEP standardised.

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from datetime import datetime
 from flask import Flask, render_template, request, url_for, redirect, session, send_file
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 
-
 app = Flask(__name__)
 app.config.update(
     SECRET_KEY='WOW SUCH SECRET'
@@ -29,7 +28,6 @@ def load_user(user_login):
     return User(user_login)
 
 
-
 def check_rights():
     if 'login' in session:
         if session['login'] == 'AdminAccess1812':
@@ -43,11 +41,9 @@ def check_rights():
     return rights
 
 
-
 @app.route('/')
 def index():
     return render_template('index.html', rights=check_rights())
-
 
 
 @app.route('/info/')
@@ -55,12 +51,10 @@ def info():
     return render_template('info.html', rights=check_rights())
 
 
-
 @app.route('/events/')
 def events():
     data = db.events.get_all()
     return render_template('events.html', rights=check_rights(), data=data)
-
 
 
 @app.route('/events/<event>', methods=['GET', 'POST'])
@@ -73,9 +67,9 @@ def event(event):
         return redirect(url_for('submit'))
 
 
-
 def hash_password(plain_text_password):
     return bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt())
+
 
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
@@ -108,9 +102,9 @@ def register():
         return render_template('register.html', rights=check_rights(), message='Регистрация прошла успешно')
 
 
-
 def check_password(plain_text_password, hashed_password):
     return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_password)
+
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
@@ -132,7 +126,6 @@ def login():
             return render_template('login.html', rights=check_rights(), error='Неправильный логин или пароль')
 
 
-
 @app.route('/edit/', methods=['GET', 'POST'])
 @login_required
 def edit():
@@ -148,7 +141,8 @@ def edit():
         row = db.users.get('login', request.form['login'])
         if row:
             if request.form['login'] != session['login']:
-                return render_template('edit.html', rights=check_rights(), message='Такой пользователь уже зарегистрирован!', data=data)
+                return render_template('edit.html', rights=check_rights(),
+                                       message='Такой пользователь уже зарегистрирован!', data=data)
 
         if not re.match(r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)', request.form['login']):
             return render_template('edit.html', rights=check_rights(), message='Неправильный формат почты', data=data)
@@ -170,11 +164,13 @@ def edit():
         return render_template('edit.html', rights=check_rights(), message='Ваши изменения сохранены!', data=data)
 
 
-
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'doc', 'docx'}
+
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route("/submit/", methods=['GET', 'POST'])
 @login_required
@@ -191,7 +187,8 @@ def submit():
         event_name = ''
         if session['event']:
             event_name = session['event']
-        return render_template('submit.html', rights=check_rights(), events=events, themes=themes, event_name=event_name)
+        return render_template('submit.html', rights=check_rights(), events=events, themes=themes,
+                               event_name=event_name)
 
     if request.method == 'POST':
         event = db.events.get('name', request.form['event'])[0]
@@ -199,20 +196,24 @@ def submit():
         exp_date = datetime.strptime(event.exp_date, "%d.%m.%Y")
 
         if datetime.now() < begin_date or datetime.now() > exp_date:
-            return render_template('submit.html', rights=check_rights(), message='По этому мероприятию работы не принимаются', events=events, themes=themes)
+            return render_template('submit.html', rights=check_rights(),
+                                   message='По этому мероприятию работы не принимаются', events=events, themes=themes)
 
         if 'file' not in request.files:
-            return render_template('submit.html', rights=check_rights(), message='Загрузите файл!', events=events, themes=themes)
+            return render_template('submit.html', rights=check_rights(), message='Загрузите файл!', events=events,
+                                   themes=themes)
 
         file = request.files['file']
         if file.filename == '':
-            return render_template('submit.html', message='Загрузите файл!', rights=check_rights(), events=events, themes=themes)
+            return render_template('submit.html', message='Загрузите файл!', rights=check_rights(), events=events,
+                                   themes=themes)
 
         if file and allowed_file(file.filename):
             user_submits = db.submits.get('login', session['login'])
             for user_submit in user_submits:
                 if user_submit.event == request.form['event'] and user_submit.theme == request.form['theme']:
-                    return render_template('submit.html', rights=check_rights(), message='Вы уже участвовали в этой номинации', events=events, themes=themes)
+                    return render_template('submit.html', rights=check_rights(),
+                                           message='Вы уже участвовали в этой номинации', events=events, themes=themes)
 
             id = len(db.submits.get_all()) + 1
             db.submits.put({'id': id,
@@ -233,11 +234,13 @@ def submit():
             else:
                 db.users.update('login', session['login'], 'participation', rows.participation + participation)
 
-            send_email.participation(session['login'], db.events.get('name', request.form['event'])[0].title, request.form['theme'], str(id))
-            return render_template('submit.html', rights=check_rights(), message='Успешно загружено', events=events, themes=themes)
+            send_email.participation(session['login'], db.events.get('name', request.form['event'])[0].title,
+                                     request.form['theme'], str(id))
+            return render_template('submit.html', rights=check_rights(), message='Успешно загружено', events=events,
+                                   themes=themes)
 
-        return render_template('submit.html', rights=check_rights(), message='Неверный формат файла', events=events, themes=themes)
-
+        return render_template('submit.html', rights=check_rights(), message='Неверный формат файла', events=events,
+                               themes=themes)
 
 
 @app.route('/participants/')
@@ -249,7 +252,6 @@ def participants():
     data.pop(0)
     data.pop(0)
     return render_template('participants.html', rights=check_rights(), data=data)
-
 
 
 @app.route('/review/', methods=['GET', 'POST'])
@@ -267,9 +269,10 @@ def review():
         data = db.submits.get_all()
         event, theme = request.form['event'], request.form['theme']
         session['jury'] = request.form['jury']
-        return render_template('review.html', rights=check_rights(), events=events, event=event, themes=themes, theme=theme, data=data, visibility='visible', jury_name=request.form['jury'], event_name=request.form['event'], theme_name=request.form['theme'])
+        return render_template('review.html', rights=check_rights(), events=events, event=event, themes=themes,
+                               theme=theme, data=data, visibility='visible', jury_name=request.form['jury'],
+                               event_name=request.form['event'], theme_name=request.form['theme'])
     return render_template('review.html', rights=check_rights(), events=events, themes=themes, visibility='hidden')
-
 
 
 @app.route('/review/download/<id>')
@@ -279,7 +282,6 @@ def download(id):
 
     upload = db.submits.get('id', id)[0]
     return send_file(BytesIO(upload.file), download_name=upload.filename, as_attachment=True)
-
 
 
 @app.route('/review/<id>', methods=['GET', 'POST'])
@@ -293,7 +295,8 @@ def review_check(id):
         checks = db.reviews.get('id', int(id))
         for check in checks:
             if check.id == int(id) and session['jury'] == check.jury:
-                return render_template('review_id.html', rights=check_rights(), data=data, message='Вы уже проверили эту работу')
+                return render_template('review_id.html', rights=check_rights(), data=data,
+                                       message='Вы уже проверили эту работу')
 
         criteria1 = int(request.form['criteria1'])
         criteria2 = int(request.form['criteria2'])
@@ -323,7 +326,6 @@ def review_check(id):
     return render_template('review_id.html', rights=check_rights(), data=data)
 
 
-
 @app.route('/rating/', methods=['GET', 'POST'])
 def rating():
     if not check_rights()['admin']:
@@ -350,29 +352,31 @@ def rating():
                     score += review.sum
 
                 final_score = score / len(reviews)
-                final_score = decimal.Decimal(final_score).quantize(decimal.Decimal('0.00'), rounding=decimal.ROUND_HALF_UP)
+                final_score = decimal.Decimal(final_score).quantize(decimal.Decimal('0.00'),
+                                                                    rounding=decimal.ROUND_HALF_UP)
 
                 existence = db.rating.get('submit_id', sumbit.id)
                 if existence:
                     db.rating.update('submit_id', sumbit.id, 'final_score', final_score)
                 else:
                     data = {
-                    'login': user.login,
-                    'last_name': user.last_name,
-                    'first_name': user.first_name,
-                    'middle_name': user.middle_name,
-                    'submit_id': sumbit.id,
-                    'university': user.university,
-                    'event': sumbit.event,
-                    'theme': sumbit.theme,
-                    'scientific_director': sumbit.scientific_director,
-                    'final_score': final_score
+                        'login': user.login,
+                        'last_name': user.last_name,
+                        'first_name': user.first_name,
+                        'middle_name': user.middle_name,
+                        'submit_id': sumbit.id,
+                        'university': user.university,
+                        'event': sumbit.event,
+                        'theme': sumbit.theme,
+                        'scientific_director': sumbit.scientific_director,
+                        'final_score': final_score
                     }
 
                     db.rating.put(data)
         data = db.rating.get_all()
-        return render_template('rating.html', rights=check_rights(), events=events, event=event, themes=themes, theme=theme, data=data, visibility='visible', event_name=request.form['event'], theme_name=request.form['theme'])
-
+        return render_template('rating.html', rights=check_rights(), events=events, event=event, themes=themes,
+                               theme=theme, data=data, visibility='visible', event_name=request.form['event'],
+                               theme_name=request.form['theme'])
 
 
 @app.route("/logout/")
@@ -385,7 +389,6 @@ def logout():
     session.pop('jury', None)
     session.pop('event', None)
     return redirect(url_for('login'))
-
 
 
 if __name__ == "__main__":
